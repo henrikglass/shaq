84a0d9fd88965068c4373a9f6a017970015ccbae
Henrik Andersson Glass <henrik.asson@gmail.com>
2025-08-05 22:17:52 +0200

    Update hgl_int.h

--- include/hgl_int.h
+++ include/hgl_int.h
@@ -5,15 +5,16 @@
 #include <stdbool.h>
 #include <assert.h>
 
-typedef bool      b8;
-typedef uint8_t   u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef int8_t    i8;
-typedef int16_t  i16;
-typedef int32_t  i32;
-typedef int64_t  i64;
+typedef bool           b8;
+typedef uint8_t        u8;
+typedef uint16_t      u16;
+typedef uint32_t      u32;
+typedef uint64_t      u64;
+typedef int8_t         i8;
+typedef int16_t       i16;
+typedef int32_t       i32;
+typedef int64_t       i64;
+typedef unsigned int uint;
 
 static_assert(sizeof(bool) == 1, "");
 
